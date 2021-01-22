8f18425e4d18b4676b828dac517b9f698442121e
Windom WU <drw_08@hotmail.com>
2021-01-22 23:04:50 +0800

    update chocolatey offline install process

---
+++ Chocolatey/runThis.bat
@@ -0,0 +1,4 @@
+set "scriptpath=%~dp0"
+set script=%scriptpath%ChocolateyInstall.ps1
+
+powershell -ExecutionPolicy bypass -command "start-process powershell -argumentlist {-ExecutionPolicy bypass -file %script%} -verb runas"
